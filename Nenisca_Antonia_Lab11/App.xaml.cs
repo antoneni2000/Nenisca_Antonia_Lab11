d48a77e9d3b61ba3c14d7ea5445c7c268c44e0b4
antoneni2000 <neniscaaa@yahoo.com>
2024-12-21 13:31:40 +0200

    Add project files.

---
+++ Nenisca_Antonia_Lab11/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Nenisca_Antonia_Lab11
+
+using Nenisca_Antonia_Lab11.Data;
+
+namespace Nenisca_Antonia_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Nenisca_Antonia_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Nenisca_Antonia_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
